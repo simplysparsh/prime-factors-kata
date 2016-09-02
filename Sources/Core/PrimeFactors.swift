94eaa526a183bcd042b5773dffbe9dee129d4ec8
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-09-02 00:45:49 -0400

    Add minor correction

--- Sources/Core/PrimeFactors.swift
+++ Sources/Core/PrimeFactors.swift
@@ -6,7 +6,7 @@ public class PrimeFactors {
     var num = number
     var divisor = 2
 
-    while num > 1 && divisor < num {   
+    while num > 1 {   
       while (num % divisor) == 0 {
         factors.append(divisor)
         num = num/divisor
@@ -14,10 +14,7 @@ public class PrimeFactors {
       divisor = divisor + 1
     } 
       
-    if num > 1 {
-      factors.append(num)
-    }  
-
+    if num > 1 { factors.append(num) }
     return factors
   }
   
